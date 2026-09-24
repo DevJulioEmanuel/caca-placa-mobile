8693b634d33317e200462c06ad6972a4d4223e23
emanueldias01 <emanueldias01dev@gmail.com>
2026-09-24 16:57:31 -0300

    add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+
+    implementation(libs.osmdroid.android)
+    implementation(libs.osm.android.compose)
 }
